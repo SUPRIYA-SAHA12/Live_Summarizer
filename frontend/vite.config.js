75f93c54f70fef1827fb5a6641ced8f722cd968d
Supriya Saha <swapnasaha814@gmail.com>
2026-04-06 18:43:28 +0530

    add docs again

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -5,4 +5,5 @@ import react from '@vitejs/plugin-react'
 export default defineConfig({
   plugins: [react()],
   content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
+  base:'/Live_Summarizer/',
 })
